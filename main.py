76df358073c8acb4c767193d338dd4447067a2ea
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-07-02 14:57:09 -0400

    Added initial code to create a sample expenses table to work with.

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import random as rand
+import numpy as npy
+import matplotlib as mat
+from datetime import datetime as dt
+from datetime import date
+
+# set up varibale to get the year
+
+today = date.today()
+
+# test = date(2022,7,2) #this will get nows date
+
+sampletable = {
+    'DATE (YYYY-MM-DD)': [today, today, today, today, today]
+    , 'INCOME': [3000,3000,3000,3000,3000]
+    , 'EXPENSES': [rand.randint(100, 500), rand.randint(100, 500), rand.randint(100, 500), rand.randint(100, 500), rand.randint(100, 500),]
+    , 'SAVINGS': [rand.randint(1, 100), rand.randint(1, 50), rand.randint(1, 20), rand.randint(1, 100), rand.randint(1, 100)]
+}
+df = pd.DataFrame(data=sampletable)
+df['TOTAL'] = df['INCOME'] - df['EXPENSES'] - df['SAVINGS']
+
+# df.to_csv('sampeldata.csv', index=False)
+
+
+print(df)
