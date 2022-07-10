0a48e56ab79b7df60fd595c62d005ee5d48f06aa
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-07-10 17:09:54 -0400

    Fixed the csv file name. Added simple code to get user data and append a new row to the file.

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ from datetime import timedelta
 
 today = date.today()
 database = pd.read_csv('sampledata.csv')
-
-
-
 print(database)
 
 
@@ -22,26 +19,36 @@ new_income: int
 new_expense: int
 new_savings: int
 new_total: int
+choice: str
 
 # get user input for new row in db
 
+choice = input("Do you want to enter a new row? ")
 
-new_income = int(input("Enter new income: $"))
-new_expense = int(input("Enter new expense: $"))
-new_savings = int(input("Enter savings: $"))
-new_total = new_income - new_expense - new_savings
+while True:
 
 
+    if choice.upper() == 'N':
+        break
+    else:
+        new_income = int(input("Enter new income: $"))
+        new_expense = int(input("Enter new expense: $"))
+        new_savings = int(input("Enter savings: $"))
+        new_total = new_income - new_expense - new_savings
 
-new_db_entry = pd.DataFrame({
-    "DATE (YYYY-MM-DD)": [today],
-    'INCOME': [new_income],
-    'EXPENSES': [new_expense],
-    'SAVINGS': [new_savings],
-    'TOTAL': [new_total]
-}, index=False)
+        new_db_entry = pd.DataFrame({
+            "DATE (YYYY-MM-DD)": [today],
+            'INCOME': [new_income],
+            'EXPENSES': [new_expense],
+            'SAVINGS': [new_savings],
+            'TOTAL': [new_total]
+        })
 
-new_db_entry.to_csv('sampledata.csv', mode='a', index=False, header=False)
+        new_db_entry.to_csv('sampledata.csv', mode='a', index=False, header=False)
+        print(f"You entered: {new_db_entry}")
+
+        choice = input("Would you like to enter another row? ")
+
 
 print("\n\nHere is the new data in the database:\n\n")
 print(pd.read_csv('sampledata.csv'))
